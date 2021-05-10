25ae143cf09bfb138878cd1803649799a9d60761
Moses Kaumatule <kaumatule93@gmail.com>
2021-05-09 22:08:03 -0600

    update navbar and responsiveness

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,11 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React, { useState, useEffect } from "react";
-import { FaBars } from "react-icons/fa";
+import {
+  FaBars,
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram
+} from "react-icons/fa";
 import { animateScroll as scroll } from "react-scroll";
 import { Link as LinkS } from 'react-scroll'
 import './Navbar.scss'
@@ -27,7 +33,7 @@ const Navbar = (props) => {
 
   return (
     <>
-      <div className='nav-top-bar'></div>
+      {/* <div className='nav-top-bar'></div> */}
       <div scrollNav={scrollNav} className='nav'>
         <div className='nav-wrap'>
           {/* <NavLogo to="home" onClick={toggleHome} scrollNav={scrollNav}>
@@ -37,59 +43,66 @@ const Navbar = (props) => {
             <FaBars />
           </div>
           <div className='nav-menu'>
-            <div className='nav-item'>
-              <LinkS
-                className='nav-link'
-                to="display"
-                smooth={true}
-                duration={750}
-                spy={true}
-                exact="true"
-                offset={-80}
-                delay={100}
-              >
-                FLAVORS
-              </LinkS>
+            <div className='nav-menu-top'>
+              <a href='http://facebook.com/cheefasfriedpies' target='_blank'><FaFacebookF/></a>
+              <a href='http://facebook.com/cheefasfriedpies' target='_blank'><FaTwitter/></a>
+              <a href='http://facebook.com/cheefasfriedpies' target='_blank'><FaInstagram/></a>
             </div>
-            <div className='nav-item'>
-              <LinkS
-                className='nav-link'
-                to="experience"
-                smooth={true}
-                duration={750}
-                spy={true}
-                exact="true"
-                offset={-80}
-                delay={100}
-              >
-                PICKUP
-              </LinkS>
-            </div>
-            <div className='nav-item'>
-              <LinkS
-                className='nav-link'
-                to="about"
-                smooth={true}
-                duration={750}
-                spy={true}
-                exact="true"
-                offset={-80}
-              >
-                ABOUT
-              </LinkS>
-            </div>
-            <div className='nav-item'>
-              <LinkS
-                className='nav-link'
-                to="contact"
-                smooth={true}
-                duration={750}
-                spy={true}
-                exact="true"
-                offset={-80}
-              >
-                CONTACT
-              </LinkS>
+            <div className='nav-menu-bottom'>
+              <div className='nav-item'>
+                <LinkS
+                  className='nav-link'
+                  to="display"
+                  smooth={true}
+                  duration={750}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                  delay={100}
+                >
+                  FLAVORS
+                </LinkS>
+              </div>
+              <div className='nav-item'>
+                <LinkS
+                  className='nav-link'
+                  to="experience"
+                  smooth={true}
+                  duration={750}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                  delay={100}
+                >
+                  PICKUP
+                </LinkS>
+              </div>
+              <div className='nav-item'>
+                <LinkS
+                  className='nav-link'
+                  to="about"
+                  smooth={true}
+                  duration={750}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                >
+                  ABOUT
+                </LinkS>
+              </div>
+              <div className='nav-item'>
+                <LinkS
+                  className='nav-link'
+                  to="contact"
+                  smooth={true}
+                  duration={750}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                >
+                  CONTACT
+                </LinkS>
+              </div>
             </div>
           </div>
           <div className='nav-order'>
@@ -98,6 +111,8 @@ const Navbar = (props) => {
             </a>
           </div>
         </div>
+        {/* <div class='verticle-line'></div> */}
+        {/* <div class='verticle-line'></div> */}
       </div>
     </>
   );
